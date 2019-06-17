193603a500bfcd86b83817ebe79d6691c498cff9
YongJe Jung <ryviusm6@gmail.com>
2019-06-17 23:07:09 +0900

    POST request 구현 중: OAuth 인증 필요
    Signed-off-by: YongJe Jung <ryviusm6@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-const request = require('request')
 require('dotenv').config()
+const rp = require('request-promise')
 
 const access_token = process.env.ACCESS_TOKEN
-console.log(access_token)
+const band_key = process.env.BAND_KEY_DEV
+const content = `
+밴드 글 입력 테스트
+`
 
-const URI = `https://openapi.band.us/v2.1/bands?access_token=${access_token}`
-request(URI, function(error, response, body) {
-  console.error('error:', error) // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-  console.log('body:', body) // Print the HTML for the Google homepage.
-})
+// const URI = `https://openapi.band.us/v2.1/bands`
+
+// const options = {
+//   uri: 'https://openapi.band.us/v2.1/bands',
+//   qs: {
+//     access_token,
+//     band_key,
+//     content
+//   },
+//   headers: {
+//     'User-Agent': 'Request-Promise'
+//   },
+//   json: true // Automatically parses the JSON string in the response
+// }
+
+const options = {
+  method: 'POST',
+  uri: 'https://openapi.band.us/v2.2/band/post/create',
+  body: {
+    access_token,
+    band_key,
+    content
+  },
+  json: true // Automatically stringifies the body to JSON
+}
+
+rp(options)
+  .then(res => {
+    // console.log('User has %d repos', res.length)
+    console.log(res)
+  })
+  .catch(err => {
+    // API call failed...
+    console.log(err)
+  })
